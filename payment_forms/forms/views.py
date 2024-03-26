29d67ef0aa55820e959f9a92f4b3d2792ea22a33
jisdtn <122818010+jisdtn@users.noreply.github.com>
2024-03-26 09:50:01 +0100

    change views.py file

--- payment_forms/forms/views.py
+++ payment_forms/forms/views.py
@@ -1,21 +1,23 @@
 import os
 
-from django.shortcuts import render
-
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 import stripe
+from django.conf import settings
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from dotenv import load_dotenv
 
 from .models import Item
-from django.conf import settings
-from dotenv import load_dotenv
 
 load_dotenv()
 
 stripe.api_key = os.getenv('stripe.api_key')
 
+
 def success_view(request):
     return render(request, 'success.html')
+
+
 @csrf_exempt
 def create_checkout_session(request, item_id):
     """Creation of payment session with API Stripe"""
@@ -34,7 +36,7 @@ def create_checkout_session(request, item_id):
             'quantity': 1,
         }],
         mode='payment',
-        success_url = 'http://localhost:8000/success'
+        success_url='http://localhost:8000/success'
     )
     return JsonResponse({'session_id': session.id})
 
@@ -42,5 +44,11 @@ def create_checkout_session(request, item_id):
 def item_detail(request, item_id):
     """Get the item detail"""
     item = Item.objects.get(id=item_id)
-    return render(request, 'item_detail.html', {'item': item, 'stripe_publishable_key': settings.STRIPE_PUBLISHABLE_KEY})
-
+    return render(
+        request,
+        'item_detail.html',
+        {
+            'item': item,
+            'stripe_publishable_key': settings.STRIPE_PUBLISHABLE_KEY
+        }
+    )
